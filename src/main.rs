e4096532b50e4e178511b090c5d6ffe016098d9d
psyashes <43512814+psyashes@users.noreply.github.com>
2021-01-01 04:27:09 +0900

    Fix sql syntax

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
 use thiserror::Error;
 use askama::Template;
-use chrono::{Utc, Local, DateTime, Date};
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
@@ -35,7 +34,7 @@ struct ProgressLog {
 #[get("/")]
 async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
     let conn = db.get()?;
-    let mut statement = conn.prepare("SELECT id, text, created_at FROM progress_log")?;
+    let mut statement = conn.prepare("SELECT id, text, created_at FROM progress_logs")?;
     let rows = statement.query_map(params![], |row| {
         let id = row.get(0)?;
         let text = row.get(1)?;
@@ -56,22 +55,22 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    let manager = SqliteConnectionManager::file("todo.db");
+    let manager = SqliteConnectionManager::file("progress_log.db");
     let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
     let conn = pool
         .get()
         .expect("Failed to get the connection from the pool.");
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS todo (
+        "CREATE TABLE IF NOT EXISTS progress_logs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             text TEXT NOT NULL,
-            created_at TEXT NOT NULL,
+            created_at TEXT NOT NULL
         )",
         params![],
     )
-    .expect("Failed to create a table `todo`.");
+    .expect("Failed to create a table `progress_logs`.");
 
-    HttpServer::new(move || App::new().service(index))
+    HttpServer::new(move || App::new().service(index).data(pool.clone()))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;

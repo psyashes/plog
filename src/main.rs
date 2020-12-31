34e873c9f8543cc2c6e798e99f081e6b5a6628b2
psyashes <43512814+psyashes@users.noreply.github.com>
2020-12-31 17:25:44 +0900

    Add log struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
 use thiserror::Error;
+use askama::Template;
+use chrono::{Utc, Local, DateTime, Date};
 
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<ProgressLog>,
+}
 
 impl ResponseError for MyError {}
 
+struct ProgressLog {
+    id: u32,
+    text: String,
+    created_at: DateTime<Local>,
+}
+
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello World!";
-
-    Ok(HttpResponse::Ok().body(response_body))
+    let mut entries = Vec::new();
+    entries.push(ProgressLog {
+        id: 1,
+        text: "My progress log".to_string(),
+        created_at: Local::now(),
+    });
+    entries.push(ProgressLog {
+        id: 2,
+        text: "I did some tasks.".to_string(),
+        created_at: Local::now(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
